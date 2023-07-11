2de532c6f2d7a9059c1b921f17eb2d9e7083c228
elaifa <mouezelaifa@gmail.com>
2023-07-11 14:00:53 -0700

    0x0B. Python - Input/Output

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,40 +1,48 @@
 #!/usr/bin/python3
 """
-reads stdin line by line and computes metrics
+Reads lines from stdin and prints status codes
+and number of times status code occured after every 10 lines
 """
-import sys
 
-file_size = 0
-status_tally = {"200": 0, "301": 0, "400": 0, "401": 0,
-                "403": 0, "404": 0, "405": 0, "500": 0}
-i = 0
-try:
-    for line in sys.stdin:
-        tokens = line.split()
-        if len(tokens) >= 2:
-            a = i
-            if tokens[-2] in status_tally:
-                status_tally[tokens[-2]] += 1
-                i += 1
-            try:
-                file_size += int(tokens[-1])
-                if a == i:
-                    i += 1
-            except FileNotFoundError:
-                if a == i:
-                    continue
-        if i % 10 == 0:
-            print("File size: {:d}".format(file_size))
-            for key, value in sorted(status_tally.items()):
-                if value:
-                    print("{:s}: {:d}".format(key, value))
-    print("File size: {:d}".format(file_size))
-    for key, value in sorted(status_tally.items()):
-        if value:
-            print("{:s}: {:d}".format(key, value))
+if __name__ == '__main__':
+    import fileinput
+    import sys
 
-except KeyboardInterrupt:
-    print("File size: {:d}".format(file_size))
-    for key, value in sorted(status_tally.items()):
-        if value:
-            print("{:s}: {:d}".format(key, value))
+    def print_dict(status, size):
+        """Prints a dictionary in sorted order"""
+        print("File size: {}".format(size))
+        status = dict(sorted(status.items()))
+        for elem in status:
+            print("{}: {}".format(elem, status[elem]))
+
+    try:
+        i = 0
+        file_size = 0
+        status_code = {}
+        new_list = [200, 301, 400, 401, 403, 404, 405, 500]
+        for line in fileinput.input():
+            if i % 10 == 0 and i != 0:
+                print_dict(status_code, file_size)
+            line = line.split()
+            if len(line) < 2:
+                continue
+            tmp = line[-2]
+            if ord(tmp[0]) not in list(range(48, 57)):
+                raise
+                continue
+            code = line[-2]
+            size = line[-1]
+            if not code:
+                continue
+            if int(code) not in new_list:
+                continue
+            if code not in status_code:
+                status_code[code] = 1
+            else:
+                status_code[code] += 1
+            file_size += int(size)
+            i += 1
+        print_dict(status_code, file_size)
+    except (IndexError, KeyboardInterrupt):
+        print_dict(status_code, file_size)
+        sys.exit(1)
