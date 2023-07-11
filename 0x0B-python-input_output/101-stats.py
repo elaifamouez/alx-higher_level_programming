1c097b640f86b4326cd2e6d8c10d6ac437d5dedb
elaifa <mouezelaifa@gmail.com>
2023-07-11 12:43:56 -0700

    0x0B. Python - Input/Output

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,48 +1,57 @@
 #!/usr/bin/python3
-"""
-Reads lines from stdin and prints status codes
-and number of times status code occured after every 10 lines
-"""
+""" Module to print status code """
+import sys
 
-if __name__ == '__main__':
-    import fileinput
-    import sys
 
-    def print_dict(status, size):
-        """Prints a dictionary in sorted order"""
-        print("File size: {}".format(size))
-        status = dict(sorted(status.items()))
-        for elem in status:
-            print("{}: {}".format(elem, status[elem]))
+class Magic:
+    """ Class to generates instances with dict and size"""
+    def __init__(self):
+        """ Init method """
+        self.dic = {}
+        self.size = 0
+
+    def init_dic(self):
+        """ Initialize dict """
+        self.dic['200'] = 0
+        self.dic['301'] = 0
+        self.dic['400'] = 0
+        self.dic['401'] = 0
+        self.dic['403'] = 0
+        self.dic['404'] = 0
+        self.dic['405'] = 0
+        self.dic['500'] = 0
+
+    def add_status_code(self, status):
+        """ add repeated number to the status code """
+        if status in self.dic:
+            self.dic[status] += 1
+
+    def print_info(self, sig=0, frame=0):
+        """ print status code """
+        print("File size: {:d}".format(self.size))
+        for key in sorted(self.dic.keys()):
+            if self.dic[key] is not 0:
+                print("{}: {:d}".format(key, self.dic[key]))
+
+
+if __name__ == "__main__":
+    magic = Magic()
+    magic.init_dic()
+    nlines = 0
 
     try:
-        i = 0
-        file_size = 0
-        status_code = {}
-        new_list = [200, 301, 400, 401, 403, 404, 405, 500]
-        for line in fileinput.input():
-            if i % 10 == 0 and i != 0:
-                print_dict(status_code, file_size)
-            line = line.split()
-            if len(line) < 2:
-                continue
-            tmp = line[-2]
-            if ord(tmp[0]) not in list(range(48, 57)):
-                raise
-                continue
-            code = line[-2]
-            size = line[-1]
-            if not code:
-                continue
-            if int(code) not in new_list:
-                continue
-            if code not in status_code:
-                status_code[code] = 1
-            else:
-                status_code[code] += 1
-            file_size += int(size)
-            i += 1
-        print_dict(status_code, file_size)
-    except (IndexError, KeyboardInterrupt):
-        print_dict(status_code, file_size)
-        sys.exit(1)
+        for line in sys.stdin:
+            if nlines % 10 == 0 and nlines is not 0:
+                magic.print_info()
+
+            try:
+                list_line = [x for x in line.split(" ") if x.strip()]
+                magic.add_status_code(list_line[-2])
+                magic.size += int(list_line[-1].strip("\n"))
+            except Exception:
+                pass
+            nlines += 1
+    except KeyboardInterrupt:
+        magic.print_info()
+        raise
+    magic.print_info()
