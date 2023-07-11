e59201eb6665eb343e67cb37cc33c7b79f3a26a3
elaifa <mouezelaifa@gmail.com>
2023-07-11 11:11:42 -0700

    0x0B. Python - Input/Output

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,59 +1,38 @@
 #!/usr/bin/python3
-"""Reads from standard input and computes metrics.
-
-After every ten lines or the input of a keyboard interruption (CTRL + C),
-prints the following statistics:
-    - Total file size up to that point.
-    - Count of read status codes up to that point.
-"""
+"""A script that reads stdin line by line and computes metrics"""
+from sys import stdin
 
 
-def print_stats(size, status_codes):
-    """Print accumulated metrics.
-
-    Args:
-        size (int): The accumulated read file size.
-        status_codes (dict): The accumulated count of status codes.
-    """
-    print("File size: {}".format(size))
-    for key in sorted(status_codes):
-        print("{}: {}".format(key, status_codes[key]))
+def print_statistics(file_size, status_codes):
+    """prints statistics"""
+    print("File size: {}".format(file_size))
+    for key in status_codes.keys():
+        if status_codes[key] != 0:
+            print("{}: {}".format(key, status_codes[key]))
 
 
-if __name__ == "__main__":
-    import sys
+def main():
+    """Reads stdin and computes metrics"""
 
-    size = 0
-    status_codes = {}
-    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-    count = 0
+    status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
+                    '403': 0, '404': 0, '405': 0, '500': 0}
+    file_size = 0
+    num_lines = 0
 
     try:
-        for line in sys.stdin:
-            if count == 10:
-                print_stats(size, status_codes)
-                count = 1
-            else:
-                count += 1
+        for line in stdin:
+            splited_line = line.rstrip().split("HTTP/1.1\" ")
+            scode_fsize = splited_line[-1].split(" ")
 
-            line = line.split()
+            status_codes[scode_fsize[0]] += 1
+            file_size += int(scode_fsize[-1])
 
-            try:
-                size += int(line[-1])
-            except (IndexError, ValueError):
-                pass
-
-            try:
-                if line[-2] in valid_codes:
-                    if status_codes.get(line[-2], -1) == -1:
-                        status_codes[line[-2]] = 1
-                    else:
-                        status_codes[line[-2]] += 1
-            except IndexError:
-                pass
-
-        print_stats(size, status_codes)
-
+            num_lines += 1
+            if num_lines >= 10:
+                print_statistics(file_size, status_codes)
+                num_lines = 0
     except KeyboardInterrupt:
-        print_stats(size, status_codes)
-        raise
+        print_statistics(file_size, status_codes)
+
+
+main()
