2c168ea92841528db74f70947ac368c0a61ecd98
elaifa <mouezelaifa@gmail.com>
2023-07-11 14:04:38 -0700

    0x0B. Python - Input/Output

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,48 +1,49 @@
 #!/usr/bin/python3
-"""
-Reads lines from stdin and prints status codes
-and number of times status code occured after every 10 lines
+"""Module 101-stats
+
+This Module contains a script that reads stdin line by line and
+computes a metrics
 """
 
-if __name__ == '__main__':
-    import fileinput
-    import sys
 
-    def print_dict(status, size):
-        """Prints a dictionary in sorted order"""
-        print("File size: {}".format(size))
-        status = dict(sorted(status.items()))
-        for elem in status:
-            print("{}: {}".format(elem, status[elem]))
+import sys
+
+
+def main():
+    """main script function to read input from stdin and process it"""
+    line_count = 0
+    er_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
+    file_size = 0
 
     try:
-        i = 0
-        file_size = 0
-        status_code = {}
-        new_list = [200, 301, 400, 401, 403, 404, 405, 500]
-        for line in fileinput.input():
-            if i % 10 == 0 and i != 0:
-                print_dict(status_code, file_size)
-            line = line.split()
-            if len(line) < 2:
-                continue
-            tmp = line[-2]
-            if ord(tmp[0]) not in list(range(48, 57)):
-                raise
-                continue
-            code = line[-2]
-            size = line[-1]
-            if not code:
-                continue
-            if int(code) not in new_list:
-                continue
-            if code not in status_code:
-                status_code[code] = 1
-            else:
-                status_code[code] += 1
-            file_size += int(size)
-            i += 1
-        print_dict(status_code, file_size)
-    except (IndexError, KeyboardInterrupt):
-        print_dict(status_code, file_size)
-        sys.exit(1)
+        for line in sys.stdin:
+            matches = line.split()
+            if len(matches) >= 2:
+                if matches[-2] in er_codes:
+                    er_codes[matches[-2]] += 1
+                file_size += int(matches[-1])
+                line_count += 1
+                if line_count % 10 == 0:
+                    print_metrics(file_size, er_codes)
+
+    except KeyboardInterrupt:
+        print_metrics(file_size, er_codes)
+        raise
+
+    print_metrics(file_size, er_codes)
+
+
+def print_metrics(file_size, er_codes):
+    """
+    computes the matrices of a set of 10 inputs or whatever is left after
+    signal interrupt
+    """
+    print(f"File size: {file_size}")
+    for code in sorted(er_codes.items(), key=lambda x: x[0]):
+        if code[1] > 0:
+            print(f"{code[0]}: {code[1]}")
+
+
+if __name__ == "__main__":
+    main()
