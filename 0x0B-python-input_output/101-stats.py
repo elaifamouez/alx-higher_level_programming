ef717acf2deeb56fc1cdc93fbbde70e8b495b0e0
elaifa <mouezelaifa@gmail.com>
2023-07-11 13:10:23 -0700

    0x0B. Python - Input/Output

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,40 +1,58 @@
-#!/usr/bin/python3
-"""
-reads stdin line by line and computes metrics
-"""
-import sys
+import os
+import signal
 
-file_size = 0
-status_tally = {"200": 0, "301": 0, "400": 0, "401": 0,
-                "403": 0, "404": 0, "405": 0, "500": 0}
-i = 0
-try:
-    for line in sys.stdin:
-        tokens = line.split()
-        if len(tokens) >= 2:
-            a = i
-            if tokens[-2] in status_tally:
-                status_tally[tokens[-2]] += 1
-                i += 1
-            try:
-                file_size += int(tokens[-1])
-                if a == i:
-                    i += 1
-            except FileNotFoundError:
-                if a == i:
-                    continue
-        if i % 10 == 0:
-            print("File size: {:d}".format(file_size))
-            for key, value in sorted(status_tally.items()):
-                if value:
-                    print("{:s}: {:d}".format(key, value))
-    print("File size: {:d}".format(file_size))
-    for key, value in sorted(status_tally.items()):
-        if value:
-            print("{:s}: {:d}".format(key, value))
 
-except KeyboardInterrupt:
-    print("File size: {:d}".format(file_size))
-    for key, value in sorted(status_tally.items()):
-        if value:
-            print("{:s}: {:d}".format(key, value))
+def compute_metrics():
+    # Dictionary to store total file size and count of lines by status code
+    metrics = {'total_size': 0, 'status_codes': {}}
+
+    try:
+        line_count = 0
+        for line in iter(input, ''):
+            line_count += 1
+            ip, date, request, status_code, file_size = parse_line(line)
+            
+            # Update total file size
+            metrics['total_size'] += file_size
+
+            # Update count of lines by status code
+            if status_code not in metrics['status_codes']:
+                metrics['status_codes'][status_code] = 0
+            metrics['status_codes'][status_code] += 1
+
+            # Print statistics every 10 lines
+            if line_count % 10 == 0:
+                print_statistics(metrics)
+
+    except (KeyboardInterrupt, EOFError):
+        print_statistics(metrics)
+
+
+def parse_line(line):
+    parts = line.strip().split()
+    ip = parts[0]
+    date = parts[2].strip('[]')
+    request = parts[4][1:]
+    status_code = parts[5]
+    file_size = int(parts[6])
+    return ip, date, request, status_code, file_size
+
+
+def print_statistics(metrics):
+    print("Total file size: File size:", metrics['total_size'])
+
+    # Sort and print count of lines by status code in ascending order
+    sorted_status_codes = sorted(metrics['status_codes'].items(), key=lambda x: x[0])
+    for code, count in sorted_status_codes:
+        print(f"{code}: {count}")
+
+
+def signal_handler(signum, frame):
+    # Handle the keyboard interruption (Ctrl + C)
+    print()
+    os._exit(0)
+
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, signal_handler)
+    compute_metrics()
