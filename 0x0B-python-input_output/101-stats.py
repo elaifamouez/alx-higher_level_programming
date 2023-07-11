87e52e385ffd9011f97395b19c8bbed78fef7814
elaifa <mouezelaifa@gmail.com>
2023-07-11 12:11:04 -0700

    0x0B. Python - Input/Output

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,45 +1,39 @@
 #!/usr/bin/python3
-"""script that reads stdin line by line and computes metrics"""
-
-
+"""reads stdin line by line and computes metrics"""
 import sys
 
-
-def print_stats(total_size, status_counts):
-    """prints the stats"""
-    print("File size: {:d}".format(total_size))
-    for key, value in sorted(status_counts.items()):
-        print("{}: {:d}".format(key, value))
-    sys.stdout.flush()
-
-
-total_size = 0
-valid_status = ['200', '301', '400', '401', '403', '404', '405', '500']
-status_counts = {}
-count = 0
-try:  # read lines from stdin
+file_size = 0
+status_tally = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
+i = 0
+try:
     for line in sys.stdin:
-        # Process the line here
-        if count == 10:
-            print_stats(total_size, status_counts)
-            count = 1
-        else:
-            count += 1
-        items = line.rstrip().split()  # remove newline character and split
-        try:
-            total_size += int(items[-1])
-        except (ValueError, IndexError):
-            pass
-        try:
-            status = items[-2]
-            if status in valid_status:
-                if status in status_counts:
-                    status_counts[status] += 1
-                else:
-                    status_counts[status] = 1
-        except IndexError:
-            pass
-    print_stats(total_size, status_counts)
+        tokens = line.split()
+        if len(tokens) >= 2:
+            a = i
+            if tokens[-2] in status_tally:
+                status_tally[tokens[-2]] += 1
+                i += 1
+            try:
+                file_size += int(tokens[-1])
+                if a == i:
+                    i += 1
+            except:
+                if a == i:
+                    continue
+
+        if i % 10 == 0:
+            print("File size: {:d}".format(file_size))
+            for key, value in sorted(status_tally.items()):
+                if value:
+                    print("{:s}: {:d}".format(key, value))
+    print("File size: {:d}".format(file_size))
+    for key, value in sorted(status_tally.items()):
+        if value:
+            print("{:s}: {:d}".format(key, value))
+
 except KeyboardInterrupt:
-    print_stats(total_size, status_counts)
-    raise
+    print("File size: {:d}".format(file_size))
+    for key, value in sorted(status_tally.items()):
+        if value:
+            print("{:s}: {:d}".format(key, value))
