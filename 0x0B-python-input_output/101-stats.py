ec3d67e46c056de0d4d7465dffdbf77771b7a075
elaifa <mouezelaifa@gmail.com>
2023-07-11 12:16:06 -0700

    0x0B. Python - Input/Output

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,38 +1,52 @@
 #!/usr/bin/python3
-'''
-Script for log parsing
-'''
 import sys
 
 
-if __name__ == "__main__":
-    a = []
-    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    dictio = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0,
-              "405": 0, "500": 0}
-    i = 0
-    file_size = 0
-    try:
-        for lines in sys.stdin:
-            a = lines.split(" ")
-            if "\n" in a[-1]:
-                value = a[-1][:-1]
-            else:
-                value = a[-1]
-            if len(a) > 2 and a[-2] in codes and value.isnumeric() is True:
-                dictio[a[-2]] += 1
-                file_size += int(value)
-                i += 1
-            if i % 10 == 0:
-                print("File size: {}".format(file_size))
-                for code in codes:
-                    if dictio[code] > 0:
-                        print("{}: {}".format(code, dictio[code]))
-        exit()
-    except Exception:
-        pass
-    finally:
-        print("File size: {}".format(file_size))
-        for code in codes:
-            if dictio[code] > 0:
-                print("{}: {}".format(code, dictio[code]))
+def print_info():
+    print('File size: {:d}'.format(file_size))
+
+    for scode, code_times in sorted(status_codes.items()):
+        if code_times > 0:
+            print('{}: {:d}'.format(scode, code_times))
+
+
+status_codes = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0
+}
+
+lc = 0
+file_size = 0
+
+try:
+    for line in sys.stdin:
+        if lc != 0 and lc % 10 == 0:
+            print_info()
+
+        pieces = line.split()
+
+        try:
+            status = int(pieces[-2])
+
+            if str(status) in status_codes.keys():
+                status_codes[str(status)] += 1
+        except:
+            pass
+
+        try:
+            file_size += int(pieces[-1])
+        except:
+            pass
+
+        lc += 1
+
+    print_info()
+except KeyboardInterrupt:
+    print_info()
+    raise
