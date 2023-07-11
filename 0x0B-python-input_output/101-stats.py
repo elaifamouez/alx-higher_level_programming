5ed0b2b3c44096a598f0dee87948694eabed8ca9
elaifa <mouezelaifa@gmail.com>
2023-07-11 11:58:20 -0700

    0x0B. Python - Input/Output

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,51 +1,57 @@
 #!/usr/bin/python3
-"""
-Reads from standard input and computes metrics
-"""
+""" Module to print status code """
+import sys
+
+
+class Magic:
+    """ Class to generates instances with dict and size"""
+    def __init__(self):
+        """ Init method """
+        self.dic = {}
+        self.size = 0
+
+    def init_dic(self):
+        """ Initialize dict """
+        self.dic['200'] = 0
+        self.dic['301'] = 0
+        self.dic['400'] = 0
+        self.dic['401'] = 0
+        self.dic['403'] = 0
+        self.dic['404'] = 0
+        self.dic['405'] = 0
+        self.dic['500'] = 0
+
+    def add_status_code(self, status):
+        """ add repeated number to the status code """
+        if status in self.dic:
+            self.dic[status] += 1
+
+    def print_info(self, sig=0, frame=0):
+        """ print status code """
+        print("File size: {:d}".format(self.size))
+        for key in sorted(self.dic.keys()):
+            if self.dic[key] is not 0:
+                print("{}: {:d}".format(key, self.dic[key]))
 
 
 if __name__ == "__main__":
-    import sys
-
-    stdin = sys.stdin
-
-    c = 0
-    size = 0
-    vd = ['200', '301', '400', '401', '403', '404', '405', '500']
-    st = {}
+    magic = Magic()
+    magic.init_dic()
+    nlines = 0
 
     try:
-        for line in stdin:
-            if c == 10:
-                print("File size: {}".format(size))
-                for i in sorted(st):
-                    print("{}: {}".format(i, st[i]))
-                c = 1
-            else:
-                c = c + 1
-
-            line = line.split()
+        for line in sys.stdin:
+            if nlines % 10 == 0 and nlines is not 0:
+                magic.print_info()
 
             try:
-                size = size + int(line[-1])
-            except (IndexError, ValueError):
+                list_line = [x for x in line.split(" ") if x.strip()]
+                magic.add_status_code(list_line[-2])
+                magic.size += int(list_line[-1].strip("\n"))
+            except:
                 pass
-
-            try:
-                if line[-2] in vd:
-                    if st.get(line[-2], -1) == -1:
-                        st[line[-2]] = 1
-                    else:
-                        st[line[-2]] = st[line[-2]] + 1
-            except IndexError:
-                pass
-
-        print("File size: {}".format(size))
-        for i in sorted(st):
-            print("{}: {}".format(i, st[i]))
-
+            nlines += 1
     except KeyboardInterrupt:
-        print("File size: {}".format(size))
-        for i in sorted(st):
-            print("{}: {}".format(i, st[i]))
+        magic.print_info()
         raise
+    magic.print_info()
