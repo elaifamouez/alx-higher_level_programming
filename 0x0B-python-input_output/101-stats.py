d9b4a7edaf9d24f4807df4108f38b710e429023e
elaifa <mouezelaifa@gmail.com>
2023-07-11 13:35:30 -0700

    0x0B. Python - Input/Output

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,54 +1,42 @@
-#!/usr/bin/python3
+import sys
 
-"""
-File: 101-stats.py
-Desc: This module reads stdin line by line and computes metrics
-"""
-
-
-def print_stats(size, status_codes):
+def print_stats(file_size, status_codes):
     """
-    This function prints the metrics
+    Prints the current statistics for file size and status codes.
+
+    :param file_size: The total file size.
+    :type file_size: int
+    :param status_codes: A dictionary containing the number of lines for each status code.
+    :type status_codes: dict
     """
-    print("File size: {}".format(size))
-    for key in sorted(status_codes):
-        print("{}: {}".format(key, status_codes[key]))
+    print("File size: {}".format(file_size))
+    for code in sorted(status_codes.keys()):
+        print("{}: {}".format(code, status_codes[code]))
 
-
-if __name__ == "__main__":
-    import sys
-
-    size = 0
+def process_log():
+    """
+    Processes the log data from standard input.
+    """
+    file_size = 0
     status_codes = {}
-    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-    counts = 0
+    line_count = 0
 
     try:
         for line in sys.stdin:
-            if counts == 10:
-                print_stats(size, status_codes)
-                counts = 1
-            else:
-                counts += 1
-
-            line = line.split()
-
-            try:
-                size += int(line[-1])
-            except (IndexError, ValueError):
-                pass
-
-            try:
-                if line[-2] in valid_codes:
-                    if status_codes.get(line[-2], -1) == -1:
-                        status_codes[line[-2]] = 1
-                    else:
-                        status_codes[line[-2]] += 1
-            except IndexError:
-                pass
-
-        print_stats(size, status_codes)
-
+            line_count += 1
+            parts = line.split()
+            file_size += int(parts[-1])
+            code = parts[-2]
+            if code not in status_codes:
+                status_codes[code] = 0
+            status_codes[code] += 1
+            if line_count % 10 == 0:
+                print_stats(file_size, status_codes)
     except KeyboardInterrupt:
-        print_stats(size, status_codes)
+        print_stats(file_size, status_codes)
         raise
+
+    print_stats(file_size, status_codes)
+
+if __name__ == '__main__':
+    process_log()
