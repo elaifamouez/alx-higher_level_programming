59298fcc45e02e43e8be120b0df191cce3ed9d01
mouezelaifa <mouezelaifa@gmail.com>
2023-08-16 11:00:41 -0700

    0x0E. SQL - More queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
--- creates the MySQL server user user_0d_1 and grant all priviledges
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
+-- script to create a new user in server
+-- DDL query to do so.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- query to grant perms to user
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
