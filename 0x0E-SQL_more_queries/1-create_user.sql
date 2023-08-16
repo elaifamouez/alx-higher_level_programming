7fde2e981d79fe7f6d0998e70d5e4e4c3ab97a97
mouezelaifa <mouezelaifa@gmail.com>
2023-08-16 12:41:24 -0700

    0x0E. SQL - More queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,9 @@
--- Create the MySQL user `user_0d_1` if it doesn't exist
+-- Creates the user user_0d_1 with all privileges.
 CREATE USER
     IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY "user_0d_1_pwd";
+    IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
-    ON *.*
-    TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+   ON *.*
+   TO 'user_0d_1'@'localhost'
+   IDENTIFIED BY 'user_0d_1_pwd';
+FLUSH PRIVILEGES;
