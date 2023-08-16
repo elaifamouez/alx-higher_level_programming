699df493b2410bd6eb192ec26a803230b900e21b
mouezelaifa <mouezelaifa@gmail.com>
2023-08-16 11:03:43 -0700

    0x0E. SQL - More queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,3 @@
--- script to create a new user in server
--- DDL query to do so.
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
--- query to grant perms to user
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+-- script to run msql commands
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.* TO user_0d_1@localhost
