bb782ae977f7beb59305f177fdfda7ad2915260e
mouezelaifa <mouezelaifa@gmail.com>
2023-08-17 00:20:58 -0700

    0x0E. SQL - More queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- create user and set password
 CREATE USER IF NOT EXISTS 'user_0d_i1'@'localhost';
 SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
