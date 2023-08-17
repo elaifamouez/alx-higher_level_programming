52d3ba35605602670ed46d7e6e595aa3c13001cd
mouezelaifa <mouezelaifa@gmail.com>
2023-08-17 04:43:13 -0700

    0x0E. SQL - More queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,15 +1,3 @@
--- Script that creates an user in MySQL server
--- Query to create the user 'user_0d_1' in MySQL server
-DROP USER IF EXISTS 'user_0d_1'@'localhost';
-DROP USER IF EXISTS 'user_0d_2'@'localhost';
-
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
-
-CREATE DATABASE IF NOT EXISTS user_2_db;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-SET PASSWORD FOR 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
-GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
-FLUSH PRIVILEGES;
+-- creates the MySQL server user user_0d_1 and grant all priviledges
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
