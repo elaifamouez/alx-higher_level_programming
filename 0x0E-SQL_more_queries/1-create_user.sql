a44d8bb8d90b588cd9796f235523eab635c76eb8
mouezelaifa <mouezelaifa@gmail.com>
2023-08-17 03:00:23 -0700

    0x0E. SQL - More queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
--- Script that creates an user in MySQL server
--- Query to create the user 'user_0d_1' in MySQL server
+-- This is for user creation 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION
