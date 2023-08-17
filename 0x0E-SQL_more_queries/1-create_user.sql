89c802496ce43218afed64d4ba12d00c20a3f970
mouezelaifa <mouezelaifa@gmail.com>
2023-08-17 01:32:33 -0700

    0x0E. SQL - More queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- creates the MySQL server user user_0d_1 and grant all priviledges
-DROP USER IF EXISTS user_0d_1@localhost;
+DROP USER IF EXISTS 'user_0d_1'@'localhost';
 CREATE USER IF NOT EXISTS user_0d_1@localhost;
 SET PASSWORD FOR user_0d_1@localhost = 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
