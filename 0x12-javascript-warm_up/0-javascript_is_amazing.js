61455bb769999e61d5859cb1426efb283aa47b94
mouezelaifa <mouezelaifa@gmail.com>
2023-09-12 10:52:31 -0700

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/node
-let myVar = 'Javascript is amazing';
-console.log(myVar);
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-let arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
-}
